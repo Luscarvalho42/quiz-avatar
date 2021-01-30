24188241c8f6553a86a5ba533de33010066b3df9
Luscarvalho42 <lucasdec.felizardo@gmail.com>
2021-01-30 12:19:14 -0300

    Finaliza aula 1 da imersão React

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,12 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+import { ThemeProvider } from 'styled-components'
+import db from '../db.json'
+import GlobalStyle from '../src/styles/globalStyle'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={db.theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  font-family: 'Lato', sans-serif;
+  color: ${({ theme }) => theme.colors.contrastText};
+}
+
+html,
+body { 
+  min-height: 100vh;
+}
+
+#__next {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+`
+
+export default GlobalStyle
